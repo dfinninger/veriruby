02528436a726c9ad38cb4f9c83c0bf977bef4bad
Devon C. Finninger <devonfinninger@gmail.com>
2014-05-20 01:02:11 -0500

    adds AND gate

--- libs/gates.rb
+++ libs/gates.rb
@@ -3,6 +3,29 @@
 #   Author: Devon Finninger
 #   Date:   2014-05-19
 
-module gates do
-
+module Gates
+  # - AND Definition
+  ###################
+  class And
+    @@count = 0
+    attr_accessor :input1, :input2, :output
+    attr_reader   :value, :id
+    def initialize(input1 = nil, input2 = nil, output = nil)
+      @id     = "#{self.class.name}_#{self.object_id}"
+      @input1 = input1
+      @input2 = input2
+      @output = output
+      @value  = :unknown
+      @@count += 1
+    end
+    
+    def update
+      if (@input1.value == @input2.value) and (@input1.value == :high)
+        @value = :high
+      else
+        @value = :low
+      end
+      @output.update
+    end
+  end
 end
--- libs/primitives.rb
+++ libs/primitives.rb
@@ -5,54 +5,56 @@
 
 require_relative 'exceptions'
 
-# - Wire Definition
-####################
-class Wire
-  @@wire_count = 0
-  attr_accessor :input, :output
-  attr_reader   :value, :id
-  
-  def initialize(input = nil, output = nil)
-    @id     = "#{self.class.name}_#{self.object_id}"
-    @input  = input
-    @output = output
-    @value  = @input.value
-    @@wire_count += 1
+module Primitives
+  # - Wire Definition
+  ####################
+  class Wire
+    @@count = 0
+    attr_accessor :input, :output
+    attr_reader   :value, :id
+    
+    def initialize(input = nil, output = nil)
+      @id     = "#{self.class.name}_#{self.object_id}"
+      @input  = input
+      @output = output
+      @value  = :unknown
+      @@count += 1
+    end
+    
+    def update
+      if [:high,:low,:float,:unknown].include?(@input.value)
+        @value = @input.value
+        @output.update if @output
+      else
+        raise IllegalLogicValue, "Value of '#{@input.value}' from '#{@input.id}' is unrecognized"
+      end
+    end
   end
   
-  def update
-    if [:high,:low,:float,:unknown].include?(@input.value)
-      @value = @input.value
-      @output.update if @output
-    else
-      raise IllegalLogicValue, "Value of '#{@input.value}' from '#{@input.id}' is unrecognized"
+  # - Register Definition
+  ########################
+  class Reg
+    @@count = 0
+    attr_accessor :output
+    attr_reader   :value, :id
+    
+    def initialize(output = nil, value = :unknown)
+      @id     = "#{self.class.name}_#{self.object_id}"
+      @output = output
+      @value  = value
+      @@count += 1
+    end
+    
+    def value=(new_val)
+      if [:high,:low,:float,:unknown].include?(new_val)
+        @value = new_val
+        @output.update if @output
+      else
+        raise IllegalLogicValue, "Value of '#{new_val}' is unrecognized"
+      end
     end
   end
 end
-
-# - Register Definition
-########################
-class Reg
-  @@reg_count = 0
-  attr_accessor :output
-  attr_reader   :value, :id
-  
-  def initialize(output = nil, value = :unknown)
-    @id      = "#{self.class.name}_#{self.object_id}"
-    @output = output
-    @value  = value
-  end
-  
-  def value=(new_val)
-    if [:high,:low,:float,:unknown].include?(new_val)
-      @value = new_val
-      @output.update if @output
-    else
-      raise IllegalLogicValue, "Value of '#{new_val}' is unrecognized"
-    end
-  end
-end
-
 
 
 
